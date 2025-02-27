6bb286c8adb76879e00e849385e42a698848ef90
Павел Иванов <crmprog3@mtkrosberg.ru>
2025-02-27 17:54:15 +0300

    feat: install MU implement InputField component and base logic

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,44 +1,11 @@
-import {useState} from "react";
-import {apiClient} from "../api/apiClient.ts";
+import SourceInputField from "./SourceInputField.tsx";
 
 const MainForm = () => {
-    const [sourcePath, setSourcePath] = useState<string>('')
-
-    const handleSetPath = (newPath: string, type: 'source' | 'target') => {
-        const directoryItems = apiClient.post('/sort/path', {newPath, type})
-
-        console.log(directoryItems)
-    }
 
     return (
-        <div className='flex items-center justify-center gap-5 h-screen'>
-            <div className='flex flex-col gap-1'>
-                <label className='font-bold opacity-60'>Путь к данным:</label>
-                <input type="text"
-                       style={{minWidth: '300px'}}
-                       className='border border-blue-500 rounded-sm px-2 py-1 flex-grow'
-                       placeholder='Укажите путь к исходным данным' value={sourcePath}
-                       onChange={e => setSourcePath(e.target.value)}
-                />
-                <button onClick={() => handleSetPath(sourcePath, 'source')}
-                        className='mt-2 border-b border-b-green-600 px-2 py-1'>
-                    Применить
-                </button>
-            </div>
-
-            <div className='flex flex-col gap-1'>
-                <label className='font-bold opacity-60'>Путь к данным:</label>
-                <input type="text"
-                       style={{minWidth: '300px'}}
-                       className='border border-blue-500 rounded-sm px-2 py-1 flex-grow'
-                       placeholder='Укажите путь к исходным данным' value={sourcePath}
-                       onChange={e => setSourcePath(e.target.value)}
-                />
-                <button onClick={() => handleSetPath(sourcePath, 'source')}
-                        className='mt-2 border-b border-b-green-600 px-2 py-1'>
-                    Применить
-                </button>
-            </div>
+        <div className='flex justify-center gap-5 h-screen p-3'>
+            <SourceInputField type='source' />
+            <SourceInputField type='destination' />
         </div>
     );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = ' http://localhost:8535/api/v1/'
+export const SOURCE_PLACEHOLDER = 'Укажите путь к исходным данным'
+export const DESTINATION_PLACEHOLDER = 'Укажите путь к конечной папке'
